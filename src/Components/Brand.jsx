bc660daba9f37377801531b4d673a1c3b08a3165
Fombi-Favour <fombifavour@gmail.com>
2022-10-16 15:19:42 +0100

    create brand component

--- src/Components/Brand.jsx
+++ src/Components/Brand.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import AOS from 'aos'
+import React from "react";
+import AOS from "aos";
 
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
 
-import client1 from '../assets/clients/client-1.png'
-import client2 from '../assets/clients/client-2.png'
-import client3 from '../assets/clients/client-3.jpg'
-import client4 from '../assets/clients/client-4.jpg'
-import client5 from '../assets/clients/client-5.jpg'
-import client6 from '../assets/clients/client-6.jpg'
+import client1 from "../assets/clients/client-1.png";
+import client2 from "../assets/clients/client-2.png";
+import client3 from "../assets/clients/client-3.jpg";
+import client4 from "../assets/clients/client-4.jpg";
+import client5 from "../assets/clients/client-5.jpg";
+import client6 from "../assets/clients/client-6.jpg";
 
 AOS.init();
 const Brand = () => {
   return (
-    <div>
-        
+    <div className="bg-[#f3f5fa] p-3 text-center">
+      <div className="container">
+        <div className="flex flex-col-6 flex-wrap gap-20 items-center justify-center">
+          <img className="w-[10%] inline-block p-3 transition-all" src={client1} alt="client1" />
+          <img className="w-[10%] inline-block p-3 transition-all" src={client2} alt="client2" />
+          <img className="w-[10%] inline-block p-3 transition-all" src={client3} alt="client3" />
+          <img className="w-[10%] inline-block p-3 transition-all" src={client4} alt="client4" />
+          <img className="w-[10%] inline-block p-3 transition-all" src={client5} alt="client5" />
+          <img className="w-[10%] inline-block p-3 transition-all" src={client6} alt="client6" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
